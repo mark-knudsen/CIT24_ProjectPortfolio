8377a5ac068d2fa9e6e885da3616e9e140a255fa
JacobDNielsen <jacobpeternielsen@gmail.com>
2024-09-30 12:48:20 +0200

    Update C2_Build

--- DB_Files/C2_build_framework_db.sql
+++ DB_Files/C2_build_framework_db.sql
@@ -1,63 +1,63 @@
 
-DROP TABLE IF EXISTS Member CASCADE;
-CREATE TABLE Member
+DROP TABLE IF EXISTS customer CASCADE;
+CREATE TABLE customer
 (
-serial_ID serial PRIMARY KEY,
+customer_ID serial PRIMARY KEY,
 email VARCHAR(256) NOT NULL UNIQUE,
 firstname VARCHAR(50) NOT NULL UNIQUE,
 password TEXT NOT NULL 
 );
 
-DROP TABLE IF EXISTS Member_History CASCADE;
-CREATE TABLE Member_History
+DROP TABLE IF EXISTS customer_search_history CASCADE;
+CREATE TABLE customer_search_history
 (
-serial_ID INT4 PRIMARY KEY,
-search_terms SMALLINT,
+customer_ID INT4 PRIMARY KEY,
+search_terms TEXT,
 created_at TIMESTAMP,
 
-FOREIGN KEY (serial_ID) REFERENCES Member (serial_ID)
+FOREIGN KEY (customer_ID) REFERENCES customer (customer_ID)
 
 );
 
-DROP TABLE IF EXISTS Member_Rating CASCADE;
-CREATE TABLE Member_Rating
+DROP TABLE IF EXISTS customer_Rating CASCADE;
+CREATE TABLE customer_Rating
 (
-serial_ID INT4,
+customer_ID INT4,
 title_ID VARCHAR(10),
 rating NUMERIC(3,1),
 created_at TIMESTAMP,
 
-PRIMARY KEY (serial_ID, title_ID),
-FOREIGN KEY (serial_ID) REFERENCES Member (serial_ID),
+PRIMARY KEY (customer_ID, title_ID),
+FOREIGN KEY (customer_ID) REFERENCES customer (customer_ID),
 FOREIGN KEY (title_ID) REFERENCES TITLE (title_ID)
 
 );
 
-DROP TABLE IF EXISTS Member_Title_Bookmark CASCADE;
+DROP TABLE IF EXISTS customer_Title_Bookmark CASCADE;
 
-CREATE TABLE Member_Title_Bookmark
+CREATE TABLE customer_Title_Bookmark
 (
-serial_ID INT4,
+customer_ID INT4,
 title_ID VARCHAR(10),
 created_at TIMESTAMP,
 annotation TEXT,
 
-PRIMARY KEY (serial_ID, title_ID, created_at),
-FOREIGN KEY (serial_ID) REFERENCES Member (serial_ID),
+PRIMARY KEY (customer_ID, title_ID, created_at),
+FOREIGN KEY (customer_ID) REFERENCES customer (customer_ID),
 FOREIGN KEY (title_ID) REFERENCES TITLE (title_ID)
 
 );
 
-DROP TABLE IF EXISTS Member_Person_Bookmark CASCADE;
-CREATE TABLE Member_Person_Bookmark
+DROP TABLE IF EXISTS customer_Person_Bookmark CASCADE;
+CREATE TABLE customer_Person_Bookmark
 (
-serial_ID INT4,
+customer_ID INT4,
 person_ID varchar(10),
 created_at TIMESTAMP,
 annotation TEXT, --Hvilken datatype??
 
-PRIMARY KEY (serial_ID, person_ID, created_at),
-FOREIGN KEY (serial_ID) REFERENCES Member (serial_ID),
+PRIMARY KEY (customer_ID, person_ID, created_at),
+FOREIGN KEY (customer_ID) REFERENCES customer (customer_ID),
 FOREIGN KEY (person_ID) REFERENCES person (person_ID)
 
 );
