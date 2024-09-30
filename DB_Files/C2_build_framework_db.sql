70795382479093bf786d7a03362a344a79901bde
hsahmad112 <hsahmad112@gmail.com>
2024-09-30 12:04:28 +0200

    Corrected table names

--- DB_Files/C2_build_framework_db.sql
+++ DB_Files/C2_build_framework_db.sql
@@ -8,18 +8,19 @@ firstname VARCHAR(50) NOT NULL UNIQUE,
 password TEXT NOT NULL 
 );
 
-DROP TABLE IF EXISTS MemberHistory CASCADE;
-CREATE TABLE MemberHistory
+DROP TABLE IF EXISTS Member_History CASCADE;
+CREATE TABLE Member_History
 (
 serial_ID INT4 PRIMARY KEY,
 search_terms SMALLINT,
 created_at TIMESTAMP,
 
 FOREIGN KEY (serial_ID) REFERENCES Member (serial_ID)
+
 );
 
-DROP TABLE IF EXISTS MemberRating CASCADE;
-CREATE TABLE MemberRating
+DROP TABLE IF EXISTS Member_Rating CASCADE;
+CREATE TABLE Member_Rating
 (
 serial_ID INT4,
 title_ID VARCHAR(10),
@@ -29,11 +30,12 @@ created_at TIMESTAMP,
 PRIMARY KEY (serial_ID, title_ID),
 FOREIGN KEY (serial_ID) REFERENCES Member (serial_ID),
 FOREIGN KEY (title_ID) REFERENCES TITLE (title_ID)
+
 );
 
-DROP TABLE IF EXISTS MemberTitleBookmark CASCADE;
+DROP TABLE IF EXISTS Member_Title_Bookmark CASCADE;
 
-CREATE TABLE MemberTitleBookmark
+CREATE TABLE Member_Title_Bookmark
 (
 serial_ID INT4,
 title_ID VARCHAR(10),
@@ -43,10 +45,11 @@ annotation TEXT,
 PRIMARY KEY (serial_ID, title_ID, created_at),
 FOREIGN KEY (serial_ID) REFERENCES Member (serial_ID),
 FOREIGN KEY (title_ID) REFERENCES TITLE (title_ID)
+
 );
 
-DROP TABLE IF EXISTS MemberPersonBookmark CASCADE;
-CREATE TABLE MemberPersonBookmark
+DROP TABLE IF EXISTS Member_Person_Bookmark CASCADE;
+CREATE TABLE Member_Person_Bookmark
 (
 serial_ID INT4,
 person_ID varchar(10),
@@ -56,4 +59,5 @@ annotation TEXT, --Hvilken datatype??
 PRIMARY KEY (serial_ID, person_ID, created_at),
 FOREIGN KEY (serial_ID) REFERENCES Member (serial_ID),
 FOREIGN KEY (person_ID) REFERENCES person (person_ID)
+
 );
