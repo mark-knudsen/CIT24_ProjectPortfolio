d6865ee739205c0c940844eb6e63f9acd5013e9c
JacobDNielsen <jacobpeternielsen@gmail.com>
2024-09-30 10:53:40 +0200

    Cleanup af dataPopulation
    Herunder fix af localized_title, localized_detail mm.

--- DB_Files/DataPopulation.sql
+++ DB_Files/DataPopulation.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS title CASCADE;
 /*Creating table title*/
 CREATE TABLE title
 (
@@ -14,7 +15,7 @@ isadult boolean
 /*Inserting data from tables title_basics to title*/
 INSERT INTO title(title_ID, title_type, primary_title, original_title, start_year, end_year, runtime, isadult)
 SELECT tconst, titletype, primarytitle, originaltitle, NULLIF(startyear, '')::NUMERIC, NULLIF(endyear, '')::NUMERIC, runtimeminutes, isadult
-FROM title_basics
+FROM title_basics;
 
 
 /*Creating table person*/
@@ -28,14 +29,14 @@ death_year numeric(4)
 );
 
 /*insert data from tables name_basics to person*/
-INSERT INTO person(person_ID, primary_name, birth_year,death_year) Select nconst, primaryname, NULLIF(birthyear, '')::NUMERIC, NULLIF(deathyear, '')::NUMERIC from name_basics
+INSERT INTO person(person_ID, primary_name, birth_year,death_year) Select nconst, primaryname, NULLIF(birthyear, '')::NUMERIC, NULLIF(deathyear, '')::NUMERIC from name_basics;
 
 
 /*Create table profession*/
 DROP TABLE IF EXISTS profession CASCADE;
 CREATE TABLE profession
 (
-profession_ID serial NOT NULL UNIQUE, --Denne skal være NOT NULL UNIQUE, da vi i primaryproffession specificerer den som foreign key
+profession_ID serial NOT NULL UNIQUE, --Denne skal vaere NOT NULL UNIQUE, da vi i primaryproffession specificerer den som foreign key
 profession varchar(256),
 primary key (profession_ID, profession) 
 );
@@ -61,7 +62,7 @@ SELECT distinct * from string_to_table(longprofstring, ',');
 return query select * from profession ORDER BY profession_id asc;
 end;
 $BODY$
-  LANGUAGE plpgsql VOLATILE
+  LANGUAGE plpgsql VOLATILE;
 
 
 /*creates primary_profession*/
@@ -93,7 +94,7 @@ insert into primary_profession(profession_id, person_id) select profession_id, p
 -- the reason we are using temp tables is cause I was unable to write code that was able to do the "where EXISTS" check in the second exist as rec is a record and not a table which I can use select/from/where
 return query select * from tempPrimProf; -- for debugging purposes, can be removed as long as return type is changed too
 end;
-$$
+$$;
 
 /*Creating most_relevant table*/
 DROP TABLE IF EXISTS most_relevant CASCADE;
@@ -124,7 +125,7 @@ insert into most_relevant(person_id, title_id) select per_id, tit_id from output
 -- the reason we are using temp tables is cause I was unable to write code that was able to do the "where EXISTS" check in the second exist as rec is a record and not a table which I can use select/from/where
 return query select * from most_relevant; 
 end;
-$$
+$$;
 
 
 /*create genre_list table*/
@@ -154,7 +155,7 @@ SELECT distinct * from string_to_table(longgenrestring, ',');
 return query select * from genre_list;
 end;
 $BODY$
-  LANGUAGE plpgsql VOLATILE
+  LANGUAGE plpgsql VOLATILE;
 
 
 /*create table title_genre*/
@@ -188,11 +189,10 @@ END LOOP;
 insert into title_genre(genre_id, title_id) select genre_list.genre_ID, tit_id from outputTable, genre_list where outputTable.genre = genre_list.genre;
 return query select * from title_genre; 
 end;
-$$
+$$;
 
-
+DROP TABLE IF EXISTS principal_cast;
 /*create table principal_cast*/
-
 CREATE TABLE principal_cast 
 (
 person_ID varchar(10),
@@ -213,12 +213,12 @@ select title_principals.nconst, ordering, title_principals.tconst, characters, c
 from title_principals, title_basics, name_basics 
 where title_basics.tconst = title_principals.tconst AND name_basics.nconst = title_principals.nconst;
 
+DROP TABLE IF EXISTS rating;
 /*create table rating*/
-
 CREATE TABLE rating
 (
 title_ID varchar(10) primary key,
-average_rating numeric(3,1), ---Ændret denne til 3,1, da man ellers kun kan vise max 9.9
+average_rating numeric(3,1), ---aendret denne til 3,1, da man ellers kun kan vise max 9.9
 vote_count numeric(7),
 foreign key (title_ID) references title (title_ID)
 );
@@ -227,7 +227,7 @@ foreign key (title_ID) references title (title_ID)
 insert into rating(title_ID, average_rating, vote_count) 
 select title_ratings.tconst, averagerating, numvotes 
 from title_ratings, title_basics 
-where title_basics.tconst = title_ratings.tconst
+where title_basics.tconst = title_ratings.tconst;
 
 
 
@@ -247,7 +247,7 @@ foreign key (title_ID) references title (title_ID)
 
 
 /*insert data into word_index from wi table*/
-insert into word_index(title_ID, word, field, lexeme) select wi.tconst, wi.word, wi.field, wi.lexeme from wi, title_basics where title_basics.tconst = wi.tconst
+insert into word_index(title_ID, word, field, lexeme) select wi.tconst, wi.word, wi.field, wi.lexeme from wi, title_basics where title_basics.tconst = wi.tconst;
 
 
 
@@ -260,7 +260,7 @@ plot TEXT
 );
 
 /*Inserting data from field in OMDB_Data called"plot" to Plot*/
-INSERT INTO plot(title_ID, plot) SELECT tconst, plot FROM omdb_data WHERE plot != 'N/A' AND plot IS NOT NULL 
+INSERT INTO plot(title_ID, plot) SELECT tconst, plot FROM omdb_data WHERE plot != 'N/A' AND plot IS NOT NULL;
 
 /*Creating table Poster*/
 DROP TABLE IF EXISTS poster CASCADE;
@@ -272,7 +272,7 @@ foreign key (title_ID) references title (title_ID)
 );
 
 /*Inserting data from field in OMDB_Data called "Poster" to Poster*/
-INSERT INTO poster(title_ID, poster) SELECT tconst, poster FROM omdb_data WHERE poster != 'N/A' AND poster IS NOT NULL 
+INSERT INTO poster(title_ID, poster) SELECT tconst, poster FROM omdb_data WHERE poster != 'N/A' AND poster IS NOT NULL; 
 
 /*Creating table episode_from_series*/
 DROP TABLE IF EXISTS episode_from_series CASCADE;
@@ -290,7 +290,7 @@ foreign key (series_title_ID) references title (title_ID)
 );
 
 /*Inserting data from title_episode to episode_from_series*/
-INSERT INTO episode_from_series(title_ID, series_title_ID, season_num, episode_num) SELECT tconst, parenttconst, seasonnumber, episodenumber FROM title_episode
+INSERT INTO episode_from_series(title_ID, series_title_ID, season_num, episode_num) SELECT tconst, parenttconst, seasonnumber, episodenumber FROM title_episode;
 
 
 /*Creating table localized_title */
@@ -325,12 +325,10 @@ attribute varchar(256),
 foreign key (localized_ID) references localized_title (localized_ID)
 ); 
 
-select * from localized_detail where localized_title = 'Twin Peaks'
-
 /*Inserting data from title_akas and localized_title to localized_detail*/
 INSERT INTO localized_detail(localized_ID, localized_title, language, region, type, attribute) SELECT lt.localized_ID, taka.title, taka.language, taka.region, taka.types, taka.attributes 
 FROM localized_title as lt
-JOIN title_akas taka ON lt.title_ID = taka.titleid AND lt.ordering = taka.ordering 
+JOIN title_akas taka ON lt.title_ID = taka.titleid AND lt.ordering = taka.ordering;
  
  
  
