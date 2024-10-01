2df08f7445d110449bdc584998abb47b1b6e8381
mskQI <156182962+mskQI@users.noreply.github.com>
2024-10-01 08:04:27 +0200

    triggers for insert and delete customer_rating

--- DB_Files/D_Basic_Framework_Functionality.sql
+++ DB_Files/D_Basic_Framework_Functionality.sql
@@ -37,9 +37,9 @@ $BODY$
 /************************************************RATING************************************************/
 
 
-    CREATE  PROCEDURE CreateCustomerRating(arg_customer_ID int4, arg_title_ID varchar, arg_rating numeric(3,1))
-         LANGUAGE plpgsql 
-		     as  $BODY$
+CREATE OR REPLACE PROCEDURE CreateCustomerRating(arg_customer_ID int4, arg_title_ID varchar, arg_rating numeric(3,1))
+LANGUAGE plpgsql as  
+$BODY$
     begin
     IF arg_rating > 10.0 OR arg_rating < 0 OR EXISTS(select customer_id, title_id from customer_rating where customer_id = arg_customer_id and title_id = arg_title_id) THEN
 
@@ -50,7 +50,7 @@ $BODY$
     END IF;
 
     end;
-    $BODY$
+$BODY$
 
 
 
@@ -69,11 +69,11 @@ $BODY$
 -- delete rating
 
 CREATE OR REPLACE PROCEDURE DeleteRating(arg_customer_ID int4, arg_title_ID varchar)
-   LANGUAGE plpgsql 
-  as  $BODY$
+   LANGUAGE plpgsql as  
+$BODY$
 begin
 
- IF EXISTS (SELECT 1 from customer_rating WHERE customer_ID=arg_customer_ID and title_ID=arg_title_ID) THEN
+ IF EXISTS (SELECT 1 from customer_rating WHERE customer_IDd=arg_customer_ID and title_ID=arg_title_ID) THEN
         DELETE from customer_rating WHERE customer_ID=arg_customer_ID and title_ID=arg_title_ID;
         RAISE NOTICE 'rating deleted successfully.';
     ELSE
@@ -187,9 +187,8 @@ $BODY$
 /*Searches a string and saves the search result to an ID*/
 CREATE OR REPLACE FUNCTION string_search(arg_customer_ID INT4, query TEXT)
 RETURNS TABLE(title_id VARCHAR(10), primary_title TEXT)
-    LANGUAGE plpgsql 
-    as  $$
-    
+    LANGUAGE plpgsql AS 
+$$    
   BEGIN
   INSERT INTO customer_search_history(customer_id, search_terms, created_at) values(arg_customer_ID, query, NOW());
 RETURN query (SELECT t.title_id, t.primary_title FROM title as t NATURAL JOIN plot as p WHERE position(query in t.primary_title)>0 OR position(query in p.plot)>0);
@@ -199,20 +198,71 @@ $$;
  -- get customer search history
 CREATE OR REPLACE FUNCTION GetCustomerSearchHistory(arg_customer_ID int4)
   RETURNS TABLE("customer_ID" int4, "search_terms" TEXT, "created_at" TIMESTAMP) AS $BODY$
-begin
-return query select * from customer_search_history WHERE customer_ID=arg_customer_ID ORDER BY created_at;
+BEGIN
+RETURN query SELECT * FROM customer_search_history WHERE customer_ID=arg_customer_ID ORDER BY created_at;
 --raise exception 'big error, beware';
-end;
+END;
 $BODY$
   LANGUAGE plpgsql VOLATILE 
    
   
 -- create customer search history
 CREATE PROCEDURE CreateCustomerSearchHistory(arg_customer_ID int4, arg_search_terms text)
-  LANGUAGE plpgsql 
-  as  $BODY$
-begin
-INSERT into customer_search_history values(arg_costumer_ID, arg_search_terms, CURRENT_TIMESTAMP);
-end;
+  LANGUAGE plpgsql AS  
 $BODY$
+  BEGIN
+    INSERT into customer_search_history VALUES(arg_costumer_ID, arg_search_terms, CURRENT_TIMESTAMP);
+  END;
+$BODY$
+
+
+
+/***************************************Rating trigger***************************************/
+
+
+/* Function to Update average_rating from Rating when inserting a customer_rating */
+CREATE OR REPLACE FUNCTION trigger_Update_Rating()
+RETURNS TRIGGER LANGUAGE PLPGSQL AS
+$$
+  BEGIN
+     UPDATE rating
+     SET average_rating = 
+        ((rating.average_rating * rating.vote_count) + NEW.rating) / (rating.vote_count + 1),
+        vote_count = (rating.vote_count + 1)
+     WHERE rating.title_id = NEW.title_id;
+
+     RETURN NEW;
+  END;
+$$;
+
+/* Insert customer rating trigger */
+CREATE OR REPLACE TRIGGER after_insert_customer_rating_trigger
+AFTER INSERT ON customer_rating
+FOR EACH ROW EXECUTE FUNCTION trigger_Update_Rating();
+
+
+
+
+/* Function to Update average_rating from rating when deleting a customer_rating */
+CREATE OR REPLACE FUNCTION trigger_Delete_Rating()
+RETURNS TRIGGER LANGUAGE PLPGSQL AS
+$$
+  BEGIN
+    UPDATE rating
+    SET 
+        average_rating = 
+        ((rating.average_rating * rating.vote_count) - OLD.rating) / (rating.vote_count - 1),
+        vote_count = rating.vote_count - 1
+    WHERE rating.title_id = OLD.title_id;
+
+    RETURN OLD;
+  END;
+$$;
+
+/* Delete customer rating trigger */
+CREATE OR REPLACE TRIGGER after_delete_customer_rating_trigger
+AFTER DELETE ON customer_rating
+FOR EACH ROW EXECUTE FUNCTION trigger_Delete_Rating();
+
+
 
