b49fe9340f049d9cb94d74a8364fb2651c261f44
Løven <marksvendstrupknudsen@gmail.com>
2024-10-03 19:09:20 +0200

    more cleanups
    Rearranged the code

--- DB_Files/D_Basic_Framework_Functionality.sql
+++ DB_Files/D_Basic_Framework_Functionality.sql
@@ -46,24 +46,25 @@ CREATE OR REPLACE PROCEDURE create_customer_rating(arg_customer_ID int4, arg_tit
 LANGUAGE plpgsql AS  
 $BODY$
     BEGIN
-    IF arg_rating > 10.0 OR arg_rating < 0 OR EXISTS(select customer_id, title_id FROM customer_rating WHERE customer_id = arg_customer_id AND title_id = arg_title_id) THEN
+      IF arg_rating > 10.0 OR arg_rating < 0 OR EXISTS(SELECT customer_id, title_id FROM customer_rating WHERE customer_id = arg_customer_id AND title_id = arg_title_id) THEN
 
-	    raise EXCEPTION 'Outside of rating range. min: 0.0 max: 10.0 AND/OR this customer has already rated this movie';
-    ELSE
-	    INSERT INTO customer_rating(customer_id, title_id, rating, created_at) VALUES (arg_customer_ID, arg_title_ID, arg_rating, now());
+        raise EXCEPTION 'Outside of rating range. min: 0.0 max: 10.0 AND/OR this customer has already rated this movie';
+      ELSE
+        INSERT INTO customer_rating(customer_id, title_id, rating, created_at) VALUES (arg_customer_ID, arg_title_ID, arg_rating, now());
 
-    END IF;
-
+      END IF;
     END;
 $BODY$
 
 
 -- get customer rating 
 CREATE OR REPLACE FUNCTION get_customer_rating(arg_customer_ID int4, arg_title_ID VARCHAR)
-  RETURNS TABLE("customer_ID" int4, "title_ID" VARCHAR, "rating" NUMERIC(3,1), "created_at" TIMESTAMP) AS $BODY$
+  RETURNS TABLE("customer_ID" int4, "title_ID" VARCHAR, "rating" NUMERIC(3,1), "created_at" TIMESTAMP) AS 
+$BODY$
   BEGIN
-    RETURN query SELECT * FROM customer_rating WHERE customer_ID = arg_customer_ID AND title_ID=arg_title_ID;
-
+    RETURN QUERY 
+      SELECT * FROM customer_rating 
+      WHERE customer_ID = arg_customer_ID AND title_ID=arg_title_ID;
   END;
 $BODY$
   LANGUAGE plpgsql VOLATILE 
@@ -71,9 +72,12 @@ $BODY$
   
 -- get customer rating history
 CREATE OR REPLACE FUNCTION get_customer_rating_history(arg_customer_ID int4)
-  RETURNS TABLE("customer_ID" int4, "title_ID" VARCHAR, "rating" NUMERIC(3,1), "created_at" TIMESTAMP) AS $BODY$
+  RETURNS TABLE("customer_ID" int4, "title_ID" VARCHAR, "rating" NUMERIC(3,1), "created_at" TIMESTAMP) AS 
+$BODY$
   BEGIN
-    RETURN query SELECT * FROM customer_rating WHERE customer_ID=arg_customer_ID ORDER BY created_at;
+    RETURN QUERY
+      SELECT * FROM customer_rating 
+      WHERE customer_ID=arg_customer_ID ORDER BY created_at;
   END;
 $BODY$
   LANGUAGE plpgsql VOLATILE 
@@ -84,30 +88,29 @@ CREATE OR REPLACE PROCEDURE delete_rating(arg_customer_ID int4, arg_title_ID VAR
    LANGUAGE plpgsql AS  
 $BODY$
   BEGIN
-
-   IF EXISTS (SELECT 1 FROM customer_rating WHERE customer_IDd=arg_customer_ID AND title_ID=arg_title_ID) THEN
+    IF EXISTS (SELECT 1 FROM customer_rating WHERE customer_IDd=arg_customer_ID AND title_ID=arg_title_ID) THEN
           DELETE FROM customer_rating WHERE customer_ID=arg_customer_ID AND title_ID=arg_title_ID;
           RAISE NOTICE 'rating deleted successfully.';
-      ELSE
-          RAISE EXCEPTION 'rating with customer_ID % does not exist.', arg_customer_ID;
-      END IF;
+    ELSE
+      RAISE EXCEPTION 'rating with customer_ID % does not exist.', arg_customer_ID;
+    END IF;
 
-  --DELETE from rating WHERE customer_ID=arg_ and title_ID=arg_title_ID;
+  -- DELETE from rating WHERE customer_ID=arg_ and title_ID=arg_title_ID;
   END;
 $BODY$
 
 
 /*update rating made previously for the same customer/title */
 CREATE OR REPLACE PROCEDURE update_customer_rating(arg_customer_ID int4, arg_title_ID VARCHAR, arg_rating NUMERIC(3,1))
-     LANGUAGE plpgsql AS  
+  LANGUAGE plpgsql AS  
 $BODY$
   BEGIN
 
-  if EXISTS(select customer_id, title_id FROM customer_rating WHERE customer_id = arg_customer_id AND title_id = arg_title_id AND arg_rating != rating) THEN
-    update customer_rating SET rating = arg_rating, created_at = now() WHERE customer_id = arg_customer_id AND title_id = arg_title_id;
-    ELSE
-    raise EXCEPTION 'No previous rating for title was found';
-  END IF;
+    IF EXISTS(SELECT customer_id, title_id FROM customer_rating WHERE customer_id = arg_customer_id AND title_id = arg_title_id AND arg_rating != rating) THEN
+      UPDATE customer_rating SET rating = arg_rating, created_at = now() WHERE customer_id = arg_customer_id AND title_id = arg_title_id;
+      ELSE
+      raise EXCEPTION 'No previous rating for title was found';
+    END IF;
 
   END;
 $BODY$
@@ -145,7 +148,7 @@ $$
 $$;
 
 
-/* Get  title bookmark */
+/* Get title bookmark */
 CREATE OR REPLACE FUNCTION get_title_bookmark (cust_id INT, tit_id VARCHAR)
 RETURNS TABLE(customer_id INT, title_id VARCHAR, created_at TIMESTAMP, notation TEXT)
 LANGUAGE plpgsql AS $$
@@ -203,7 +206,7 @@ RETURNS TABLE(title_id VARCHAR(10), primary_title TEXT)
 $$    
   BEGIN
     INSERT INTO customer_search_history(customer_id, search_terms, created_at) values(arg_customer_ID, query, NOW());
-    RETURN query (SELECT t.title_id, t.primary_title FROM title as t NATURAL JOIN plot as p WHERE position(query in t.primary_title)>0 OR position(query in p.plot)>0);
+    RETURN QUERY (SELECT t.title_id, t.primary_title FROM title as t NATURAL JOIN plot as p WHERE position(query in t.primary_title)>0 OR position(query in p.plot)>0);
   END;
 $$;
 
@@ -212,8 +215,10 @@ CREATE OR REPLACE FUNCTION get_customer_search_history(arg_customer_ID int4)
   RETURNS TABLE("customer_ID" int4, "search_terms" TEXT, "created_at" TIMESTAMP) AS 
 $BODY$
   BEGIN
-    RETURN query SELECT * FROM customer_search_history WHERE customer_ID=arg_customer_ID ORDER BY created_at;
-  --raise exception 'big error, beware';
+    RETURN QUERY 
+      SELECT * FROM customer_search_history 
+      WHERE customer_ID=arg_customer_ID ORDER BY created_at;
+  -- raise exception 'big error, beware';
   END;
 $BODY$
   LANGUAGE plpgsql VOLATILE 
@@ -224,55 +229,65 @@ CREATE OR REPLACE PROCEDURE create_customer_search_history(arg_customer_ID int4,
   LANGUAGE plpgsql AS  
 $BODY$
   BEGIN
-    INSERT into customer_search_history VALUES(arg_costumer_ID, arg_search_terms, CURRENT_TIMESTAMP);
+    INSERT INTO customer_search_history VALUES(arg_costumer_ID, arg_search_terms, CURRENT_TIMESTAMP);
   END;
 $BODY$
 
 
 
 -- create structured search query
-drop function structured_search_query(int4, text, text, text, text);
+DROP FUNCTION structured_search_query(int4, text, text, text, text);
 CREATE OR REPLACE FUNCTION structured_search_query(arg_user_id int4, arg_title_name text=NULL::text, arg_plot text=NULL::text, arg_character text=NULL::text, arg_person_name text=NULL::text)
-  RETURNS TABLE(title_id varchar, primary_name text) AS $BODY$
+  RETURNS TABLE(title_id varchar, primary_name text) AS 
+$BODY$
 
-declare search_term text = arg_title_name || ',' || arg_plot || ',' || arg_character || ',' || arg_person_name;
+  DECLARE search_term text = arg_title_name || ',' || arg_plot || ',' || arg_character || ',' || arg_person_name;
 
-begin 
+  BEGIN
+    IF arg_title_name = '' AND arg_plot = '' AND arg_character = '' AND arg_person_name = '' THEN
+    raise exception 'fill out the search tearm';
+    END IF;
 
-if arg_title_name = '' and arg_plot = '' and arg_character = '' and arg_person_name = '' then
-raise exception 'fill out the search tearm';
-end if;
+    INSERT INTO customer_search_history values(arg_user_ID, search_term);
 
-insert into customer_search_history values(arg_user_ID, search_term);
-
-return query Select distinct title.title_id, title.primary_title from title NATURAL join plot NATURAL join principal_cast NATURAL join person WHERE
- primary_title ilike '%'|| arg_title_name || '%' and plot.plot ilike '%' || arg_plot || '%' and principal_cast.character_name ilike '%' || arg_character || '%'
-and person.primary_name ilike '%' || arg_person_name || '%' and (principal_cast.category = 'actor' or principal_cast.category = 'actress');
-
-end;
+    RETURN QUERY 
+      SELECT DISTINCT title.title_id, title.primary_title 
+      FROM title NATURAL JOIN plot NATURAL JOIN principal_cast NATURAL JOIN person 
+      WHERE
+        primary_title ilike '%'|| arg_title_name || '%' 
+        AND plot.plot ilike '%' || arg_plot || '%' 
+        AND principal_cast.character_name ilike '%' || arg_character || '%'
+        AND person.primary_name ilike '%' || arg_person_name || '%' 
+        AND (principal_cast.category = 'actor' OR principal_cast.category = 'actress');
+  END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
   
 
 -- create structured actor search query
 CREATE OR REPLACE FUNCTION structured_actor_search_query(arg_user_id int4, arg_title_name text=NULL::text, arg_plot text=NULL::text, arg_character text=NULL::text, arg_person_name text=NULL::text)
-  RETURNS TABLE(primary_name varchar, actor_character text) AS $BODY$
+  RETURNS TABLE(primary_name varchar, actor_character text) AS 
+$BODY$
 
-declare search_term text = arg_title_name || ',' || arg_plot || ',' || arg_character || ',' || arg_person_name;
+  DECLARE search_term text = arg_title_name || ',' || arg_plot || ',' || arg_character || ',' || arg_person_name;
 
-begin 
+  BEGIN
+    IF arg_title_name = '' AND arg_plot = '' AND arg_character = '' AND arg_person_name = '' THEN
+      raise exception 'fill out the search tearm';
+    END IF;
 
-if arg_title_name = '' and arg_plot = '' and arg_character = '' and arg_person_name = '' then
-raise exception 'fill out the search tearm';
-end if;
+    INSERT INTO customer_search_history values(arg_user_ID, search_term);
 
-insert into customer_search_history values(arg_user_ID, search_term);
-
-return query Select distinct person.primary_name, principal_cast.character_name from title NATURAL join plot NATURAL join principal_cast NATURAL join person WHERE
- primary_title ilike '%'|| arg_title_name || '%' and plot.plot ilike '%' || arg_plot || '%' and principal_cast.character_name ilike '%' || arg_character || '%'
-and person.primary_name ilike '%' || arg_person_name || '%' and (principal_cast.category = 'actor' or principal_cast.category = 'actress');
-
-end;
+    RETURN QUERY 
+      Select distinct person.primary_name, principal_cast.character_name 
+      FROM title NATURAL JOIN plot NATURAL JOIN principal_cast NATURAL JOIN person 
+      WHERE
+        primary_title ilike '%'|| arg_title_name || '%' 
+        AND plot.plot ilike '%' || arg_plot || '%' 
+        AND principal_cast.character_name ilike '%' || arg_character || '%'
+        AND person.primary_name ilike '%' || arg_person_name || '%' 
+        AND (principal_cast.category = 'actor' OR principal_cast.category = 'actress');
+  END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
 
@@ -357,12 +372,12 @@ $$;
 DROP MATERIALIZED view related_title_actors;
 CREATE OR REPLACE MATERIALIZED VIEW related_title_actors AS 
 SELECT 
-  distinct person_id, 
+  DISTINCT person_id, 
   primary_name,
   primary_title,
   title_ID
 FROM 
-  title NATURAL join principal_cast NATURAL join person
+  title NATURAL JOIN principal_cast NATURAL JOIN person
 WHERE 
   principal_cast.category = 'actor' OR principal_cast.category = 'actress' 
 ORDER BY person_id;
@@ -370,36 +385,40 @@ ORDER BY person_id;
 
 
 -- added average rating to person
-alter table person add column person_average_rating numeric(3,1)
+ Alter TABLE person add column person_average_rating numeric(3,1)
 
---Alter table person add column person_average_rating numeric(3,1)
-DO $$ declare rec record;
-begin
-for rec in select * from related_title_actors natural join rating where related_title_actors.title_id = rating.title_id
-loop
-
---raise notice '%', trunc((rec.vote_count*rec.average_rating)/(rec.vote_count),1);
-update person
-SET person_average_rating = trunc((rec.vote_count*rec.average_rating)/(rec.vote_count),1) where person_id = rec.person_id;
-end loop;
-end;
+-- Alter TABLE person add column person_average_rating numeric(3,1)
+DO 
+$$ 
+  DECLARE rec record;
+  BEGIN
+    FOR rec IN SELECT * FROM related_title_actors NATURAL JOIN rating WHERE related_title_actors.title_id = rating.title_id
+    LOOP
+      -- raise notice '%', trunc((rec.vote_count*rec.average_rating)/(rec.vote_count),1);
+      UPDATE person 
+      SET person_average_rating = trunc((rec.vote_count*rec.average_rating)/(rec.vote_count),1) 
+      WHERE person_id = rec.person_id;
+    END LOOP;
+  END;
 $$
 
 
 
 /* Function to determine popular actors */
 CREATE OR REPLACE FUNCTION popular_actors(arg_movie_title TEXT)
-
 RETURNS TABLE (person_id VARCHAR, primary_name VARCHAR, person_average_rating NUMERIC)
 LANGUAGE plpgsql AS
 $$
   BEGIN 
-    RETURN query SELECT rta.person_id, rta.primary_name, p.person_average_rating FROM related_title_actors as rta NATURAL JOIN person as p  WHERE primary_title ilike '%' || arg_movie_title || '%' ORDER BY person_average_rating DESC;
-    
+    RETURN QUERY 
+      SELECT rta.person_id, rta.primary_name, p.person_average_rating 
+      FROM related_title_actors as rta NATURAL JOIN person as p  
+      WHERE primary_title ilike '%' || arg_movie_title || '%' 
+      ORDER BY person_average_rating DESC;    
   END;
 $$;
 
---Test of function popular_actors
+-- Test of function popular_actors
 SELECT * FROM popular_actors('Dinsdale!');
 
 /* Function to determine popular coplayers */
@@ -408,7 +427,7 @@ RETURNS TABLE (coplayer_ID VARCHAR, coplayer_name VARCHAR, rating NUMERIC)
 LANGUAGE plpgsql AS
 $$
   BEGIN 
-    RETURN query 
+    RETURN QUERY 
     SELECT
       t2.person_id, 
       t2.primary_name,
@@ -423,7 +442,7 @@ $$
   END;
 $$;
 
---Test of function determine_popular_coplayers
-SELECT * from determine_popular_coplayers('Masaharu Fukuyama');
-SELECT * FROM person WHERE person_id = 'nm1157364'
+-- Test of function determine_popular_coplayers
+-- SELECT * FROM determine_popular_coplayers('Masaharu Fukuyama');
+-- SELECT * FROM person WHERE person_id = 'nm1157364'
 
