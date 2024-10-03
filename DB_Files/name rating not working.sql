a865f8e36abe65ece631e572ca944d57803cac18
mskQI <156182962+mskQI@users.noreply.github.com>
2024-10-03 15:50:01 +0200

    added code for nme_rating that seems to work

--- DB_Files/name rating not working.sql
+++ DB_Files/name rating not working.sql
@@ -1,3 +1,9 @@
+/* TASK: Derive a rating of names (just actors or all names, as you prefer) based on
+ratings of the titles they are related to. Modify the database to store also these name ratings.
+Make sure to give higher influence to titles with more votes in the calculation. You can do
+this by Calculating a weighted average of the averagerating for the titles, where the
+numvotes is used as weight.*/
+
 Alter table person add column person_average_rating numeric(3,1)
 
 
@@ -19,18 +25,61 @@ end loop;
 with rating_cte as 
 (select (sum(tt.person_rating)/count(tt.person_rating)) as rat, tt.pid as avg1 from tt group by tt.pid) 
 
-update person
-set person_average_rating = rat from rating_cte where rating_cte.avg1 = person.person_ID; 
-end;
+UPDATE person SET person_average_rating = rat 
+FROM rating_cte WHERE rating_cte.avg1 = person.person_ID; 
+END;
 $$ 
 
-select sum(person_rating) from tt where pid = 'nm0000092';
-select person_average_rating from person where person_id= 'nm0000092'
-select * from tt where pid = 'nm0000092'
-select primary_name from person  where person_id='nm0000092'
-select count(person_id) from related_title_actors where person_id = 'nm5380976'
+-- if using above code the sum of John cleese is 328.9 and the average_rating is (8,0)
+SELECT pid, SUM(weighted_rating) / SUM(total_votes) AS final_rating
+FROM tt WHERE pid = 'nm0000092' GROUP BY pid;
+SELECT * FROM PERSON WHERE PERSON_ID='nm0000092';
+
+SELECT person_average_rating FROM person WHERE person_id= 'nm0000092';
+SELECT * FROM tt WHERE pid = 'nm0000092';
+SELECT sum(person_rating) from tt where pid='nm0000092';
+
+SELECT primary_name FROM person WHERE person_id='nm0000092';
+SELECT COUNT(person_id) FROM related_title_actors WHERE person_id = 'nm5380976';
 
 
-update person
-set person_average_rating = 0
-select * from person order by  person_average_rating asc
+/* TEST QUERY THAT LOOKS LIKE IT WORK! */
+DO $$ 
+DECLARE 
+    rec RECORD;
+    tempval NUMERIC;
+BEGIN
+    -- Drop and create a temporary table to store intermediate values
+    DROP TABLE IF EXISTS tt;
+    CREATE TEMP TABLE tt (pid VARCHAR, weighted_rating NUMERIC, total_votes INT);
+
+    -- Loop through the related titles and ratings
+    FOR rec IN 
+        SELECT person_id, vote_count, average_rating 
+        FROM related_title_actors 
+        NATURAL JOIN rating 
+        WHERE related_title_actors.title_id = rating.title_id
+    LOOP
+        -- Insert the weighted rating and the vote count for each person
+        INSERT INTO tt(pid, weighted_rating, total_votes) 
+        VALUES(rec.person_id, rec.vote_count * rec.average_rating, rec.vote_count);
+    END LOOP;
+
+    -- Use a CTE to calculate the weighted average for each person
+    WITH rating_cte AS (
+        SELECT 
+            pid, 
+            SUM(weighted_rating) / SUM(total_votes) AS final_rating
+        FROM tt
+        GROUP BY pid
+    )
+
+    -- Update the person's average rating in the 'person' table
+    UPDATE person
+    SET person_average_rating = rating_cte.final_rating
+    FROM rating_cte 
+    WHERE rating_cte.pid = person.person_ID;
+
+END $$;
+
+
